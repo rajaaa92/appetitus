af2010df8f53cc948a2e29b74d1f4739e76de400
rajaaa92 <rajaaa92@gmail.com>
2012-08-10 15:27:02 +0200

    c

--- db/migrate/20120807172246_drop_comments.rb
+++
@@ -1,9 +0,0 @@
-class DropComments < ActiveRecord::Migration
-  def self.up
-    drop_table :comments
-  end
-
-  def self.down
-    
-  end
-end
--- db/migrate/20120807191234_drop_cookbooks.rb
+++
@@ -1,7 +0,0 @@
-class DropCookbooks < ActiveRecord::Migration
-  def self.up
-    drop_table :cookbooks
-    
-  end
-
-end

ee794c5f39403e04a8ec03b297e467719c4fc702
rajaaa92 <rajaaa92@gmail.com>
2012-08-10 12:28:14 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appetitush::Application.config.secret_token = '8aaa60da841f182503577ab512c219ed9fe7778b9b5b7361b77a2a0e56374b037723eaa61c8432fd0b101d32e8f6d5c8e81503886fb89d8333b97063b93a498f'

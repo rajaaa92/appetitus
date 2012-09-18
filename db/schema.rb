e2bfa8c75e36a66c473c9dd35f6248ec3f863f03
rajaaa92 <rajaaa92@gmail.com>
2012-09-19 00:46:00 +0200

    default role in migration

--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,10 @@ ActiveRecord::Schema.define(:version => 20120918103658) do
   end
 
   create_table "users", :force => true do |t|
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                      :null => false
+    t.datetime "updated_at",                                      :null => false
+    t.string   "email",                  :default => "",          :null => false
+    t.string   "encrypted_password",     :default => "",          :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(:version => 20120918103658) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "role"
+    t.string   "role",                   :default => "logged_in"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

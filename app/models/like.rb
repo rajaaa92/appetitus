8303269662e644dc8ff073451d825e89ccb880bc
rajaaa92 <rajaaa92@gmail.com>
2012-10-02 20:26:02 +0200

    usun pliki2

--- app/models/like.rb
+++
@@ -1,3 +0,0 @@
-class Like < ActiveRecord::Base
-  attr_accessible :recipe_id, :user_id
-end
--- app/models/session.rb
+++
@@ -1,7 +0,0 @@
-class Session < ActiveRecord::Base
-	def after_initialize
-		if new_record?
-			user ||= 10
-		end
-	end	
-end

222a66574a291f01df19a79c1ebcd3f94921b3ee
rajaaa92 <rajaaa92@gmail.com>
2012-10-02 20:17:36 +0200

    wciecia i komentarze2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,28 +3,6 @@ class LikesController < RecipesController
 		@recipe = Recipe.find(params[:id])
 		@recipe.rate = 3
 		@recipe.save
-		#@like = Like.new
-		#@like.user_id = current_user.id
-		#@like.recipe_id = @recipe.id
-		
-		#if Like.where(:recipe_id => @recipe.id, :user_id => current_user.id) == nil
-			#respond_to do |format|
-		   # if @like.save
-					#t = @recipe.rate + 1
-					
-					#@recipe.update_attributes(rate = 3)
-		    #  format.html { redirect_to(@recipe, :notice => 'You successfully liked the recipe.') }
-		     # format.xml  { render :xml => @recipe, :status => :created, :location => @recipe }
-		    #else
-		     # format.html { redirect_to(@recipe, :notice => 
-		      #'Recipe could not be liked.')}
-		      
-		   # end
-    	#end
-		#else
-		#	format.html { redirect_to(@recipe, :notice => 
-		 #     'Recipe could not be liked. You have already like it.')}
-		#end
 
 		respond_to do |format|
       if @recipe.update_attributes(params[:recipe])
@@ -36,6 +14,4 @@ class LikesController < RecipesController
       end
 		end
   end
-
-
 end

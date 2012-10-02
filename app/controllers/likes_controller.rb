0bada6891b971da001d7542689c4e43cb6f2a832
rajaaa92 <rajaaa92@gmail.com>
2012-10-02 20:20:27 +0200

    wciecia i komentarze4

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,9 @@
 class LikesController < RecipesController 
   def like_it
-		@recipe = Recipe.find(params[:id])
-		@recipe.rate = 3
-		@recipe.save
-
-		respond_to do |format|
+    @recipe = Recipe.find(params[:id])
+    @recipe.rate = 3
+    @recipe.save
+    respond_to do |format|
       if @recipe.update_attributes(params[:recipe])
         format.html { redirect_to(@recipe, :notice => 'Recipe was successfully liked.') }
         format.xml  { head :ok }

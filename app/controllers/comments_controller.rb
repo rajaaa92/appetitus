a5ae29f16c00887d841851787e3cd7d6e592d3e1
Joanna Rajewska <joanna.rajewska@netguru.pl>
2013-10-23 20:36:08 +0100

    use expose

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,46 +1,32 @@
 class CommentsController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
 
-  def index
-    @comments = Comment.all
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-  end
-
-  def new
-    @comment = Comment.new
-  end
+  expose(:comment) { Comment.where(id: params[:id]).first || Comment.new(recipe_id: recipe.id) }
+  expose(:recipe) { Recipe.where(id: params[:recipe_id]).first }
 
   def edit
-		@recipe = Recipe.find(params[:recipe_id])
-    @comment = Comment.find(params[:id])
   end
 
   def create
-    @recipe = Recipe.find(params[:recipe_id])
-    @comment = @recipe.comments.build(params[:comment])
-		@comment.user_id = current_user.id
-    if @comment.save
-      redirect_to(@recipe, :notice => 'Comment was successfully created.')
+    comment.update_attributes(params[:comment])
+		comment.user_id = current_user.id
+    if comment.save
+      redirect_to(recipe, :notice => 'Comment was successfully created.')
     else
-      redirect_to(@recipe, :notice => 'Comment could not be saved. Please fill in all fields')
+      redirect_to(recipe, :notice => 'Comment could not be saved. Please fill in all fields')
     end
   end
 
   def update
-    @comment = Comment.find(params[:id])
-    if @comment.update_attributes(params[:comment])
-      redirect_to(@comment, :notice => 'Comment was successfully updated.')
+    if comment.update_attributes(params[:comment])
+      redirect_to(recipe, :notice => 'Comment was successfully updated.')
     else
       render :action => "edit"
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    redirect_to(comments_url)
+    comment.destroy
+    redirect_to(recipe)
   end
 end

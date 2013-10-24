845d68af55dbd66c25c097ea3984df9f5cfe472e
Joanna Rajewska <joanna.rajewska@netguru.pl>
2013-10-25 00:15:53 +0100

    trying to fix the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,4 +12,14 @@ Appetitush::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { :host => 'appetitus.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => "1appetitus@gmail.com",
+    :password             => ENV["EMAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
